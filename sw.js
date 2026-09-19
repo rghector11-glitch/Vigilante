611d08a73c44fcafa94023e31637f1a3468d46a1
rghector11-glitch <rghector11@gmail.com>
2026-09-19 15:59:40 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'vigilante-bypass-v1';
-const ASSETS = ['./vigilante-bypass.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
